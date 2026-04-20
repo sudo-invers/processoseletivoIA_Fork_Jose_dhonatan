f78825b8db261523624fde36013795b5b8a66ca5
sudo-invers <dhonatanjose8@gmail.com>
2026-04-20 14:26:37 -0300

    Tensorflow test

--- train_model.py
+++ train_model.py
@@ -1,5 +1,35 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras import layers
 
-#insira seu código aqui
+# insira seu código aqui
+
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+import tensorflow as tf
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = (
+    train_images.reshape((train_images.shape[0], 28 * 28)).astype("float32") / 255
+)
+test_images = (
+    test_images.reshape((test_images.shape[0], 28 * 28)).astype("float32") / 255
+)
+
+model = Sequential(
+    [
+        Dense(128, activation="relu", input_shape=(28 * 28,)),
+        Dense(10, activation="softmax"),
+    ]
+)
+
+model.compile(
+    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+)
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f"Test accuracy: {test_acc}")
