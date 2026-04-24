a3fe554c5179b8119490453fee63eaaa48e6ccfd
sudo-invers <dhonatanjose8@gmail.com>
2026-04-24 12:59:18 -0300

    docs: Added more coments for a better comprehension of the code

--- train_model.py
+++ train_model.py
@@ -2,15 +2,19 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
 
-tf.random.set_seed(42)
+tf.random.set_seed(42)  # Define a fixed seed for consistency of results
 
 
 def main():
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
+    # Normalizes pixels from 0-255 to 0-1
+    # stabilizes training and speeds up convergence
     train_images = train_images.astype("float32") / 255
     test_images = test_images.astype("float32") / 255
 
+    # (batch, height, width, channels)
+    # MNIST is grayscale so, 1 channel
     train_images = train_images.reshape((-1, 28, 28, 1))
     test_images = test_images.reshape((-1, 28, 28, 1))
 
@@ -29,9 +33,10 @@ def main():
     model.compile(
         optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
     )
+    # 4 Epcochs is enought, in tests, i have get a accuracy of ~99%
+    model.fit(train_images, train_labels, epochs=4, batch_size=32, verbose=2)
 
-    model.fit(train_images, train_labels, epochs=5, batch_size=32, verbose=2)
-
+    # Evaluate model performance on unseen test data
     test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=0)
 
     print(f"Test loss: {test_loss}")
