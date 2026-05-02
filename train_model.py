fe873aca9ec6ab2a14de1d9c28fcd108cf625034
sudo-invers <dhonatanjose8@gmail.com>
2026-05-02 13:38:38 -0300

    refactor(Reduced conv2d): Reduced in half, to mkae the file size small, and it don't have a significant impact in precision

--- train_model.py
+++ train_model.py
@@ -20,12 +20,12 @@ def main():
 
     model = models.Sequential(
         [
-            layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)),
+            layers.Conv2D(16, (3, 3), activation="relu", input_shape=(28, 28, 1)),
             layers.MaxPooling2D((2, 2)),
-            layers.Conv2D(64, (3, 3), activation="relu"),
+            layers.Conv2D(32, (3, 3), activation="relu"),
             layers.MaxPooling2D((2, 2)),
             layers.Flatten(),
-            layers.Dense(64, activation="relu"),
+            layers.Dense(32, activation="relu"),
             layers.Dense(10, activation="softmax"),
         ]
     )
@@ -33,7 +33,7 @@ def main():
     model.compile(
         optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
     )
-    # 4 Epcochs is enought, in tests, i have get a accuracy of ~99%
+    # Epcochs is enought, in tests, i have get a accuracy of ~99%
     model.fit(train_images, train_labels, epochs=4, batch_size=32, verbose=2)
 
     # Evaluate model performance on unseen test data
